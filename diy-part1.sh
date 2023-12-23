f5997e47bcf5433bc4eee7b3b4d1f75411330a60
Masaaki <147675004+yndzm@users.noreply.github.com>
2023-12-23 14:05:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -40,62 +40,24 @@ rm -rf feeds/packages/net/adguardhome
 #克隆passwall环境插件
 #git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall
 
-# openclash
-cd /workdir
-#预置OpenClash内核和GEO数据
-export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
-export CORE_TUN=https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux
-export CORE_DEV=https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux
-export CORE_MATE=https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux
-
-export CORE_TYPE=$(echo x86_64 | grep -Eiq "64|86" && echo "amd64" || echo "arm64")
-export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
-
-export GEO_MMDB=https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb
-export GEO_SITE=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat
-export GEO_IP=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat
-
-git clone --depth 1 https://github.com/vernesong/OpenClash.git
-
-cd OpenClash/luci-app-openclash/root/etc/openclash
-curl -sfL -o ./Country.mmdb $GEO_MMDB
-curl -sfL -o ./GeoSite.dat $GEO_SITE
-curl -sfL -o ./GeoIP.dat $GEO_IP
-
-mkdir ./core && cd ./core
-
-curl -sfL -o ./tun.gz "$CORE_TUN"-"$CORE_TYPE"-"$TUN_VER".gz
-gzip -d ./tun.gz && mv ./tun ./clash_tun
-
-curl -sfL -o ./meta.tar.gz "$CORE_MATE"-"$CORE_TYPE".tar.gz
-tar -zxf ./meta.tar.gz && mv ./clash ./clash_meta
-
-curl -sfL -o ./dev.tar.gz "$CORE_DEV"-"$CORE_TYPE".tar.gz
-tar -zxf ./dev.tar.gz
-
-chmod +x ./clash* ; rm -rf ./*.gz
-
-cd /workdir/OpenClash
-cp -r luci-app-openclash /workdir/openwrt/package/
 
 #克隆的源码放在small文件夹
-#mkdir package/small
-#pushd package/small
+mkdir package/small
+pushd package/small
 
 #克隆源码
 #passwall
 #git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
 #mosdns
-git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git package/luci-app-mosdns
+git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
 #v2ray-geodata
 #git clone https://github.com/sbwml/v2ray-geodata
 #smartdns
 #git clone --depth=1 https://github.com/pymumu/luci-app-smartdns.git
 #git clone --depth=1 https://github.com/pymumu/openwrt-smartdns.git
 #adguardhome
-git clone --depth 1 https://github.com/chenmozhijin/luci-app-adguardhome.git package/luci-app-adguardhome
-#OpenClash
-#git clone --depth 1 https://github.com/vernesong/OpenClash.git
+git clone --depth 1 https://github.com/chenmozhijin/luci-app-adguardhome.git
+git clone --depth 1 https://github.com/vernesong/OpenClash.git
 #diskman
 #git clone --depth 1 https://github.com/lisaac/luci-app-diskman.git
 #修改版本为编译日期
